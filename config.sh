b1c4b43e393b4686bc25f9bb50d2a062a7e0321b
Sushrut1101 <guptasushrut@gmail.com>
2022-12-06 16:13:59 +0530

    evergo
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- config.sh
+++ config.sh
@@ -3,40 +3,41 @@
 #
 
 # Manifest
-export MANIFEST=""
-export MANIFEST_BRANCH=""
+export MANIFEST="https://github.com/ArrowOS/android_manifest.git"
+export MANIFEST_BRANCH="arrow-12.1"
 
 # Device info
-export DEVICE=""
-export OEM=""
+export DEVICE="evergo"
+export OEM="xiaomi"
 
 # Lunch combo and Build target
-export LUNCH_COMBO=""
-export TARGET=(
-)
+export LUNCH_COMBO="arrow_evergo-eng"
+export TARGET="bacon"
 
 # Our output file
-OUTPUT=""
+OUTPUT="Arrow-*.zip"
 
 # Device trees and dependencies
-DT_LINK=""
+DT_LINK="https://github.com/Xiaomi-MT6833/device_xiaomi_evergo.git"
 DT_BRANCH=""
 
 VT_LINK=""
 
-KERNEL_SRC=""
+KERNEL_SRC="https://github.com/PixelExperience-Devices/kernel_redmi_rosemary.git"
 KERNEL_PATH="kernel/${OEM}/${DEVICE}"
 
 DEPS=(
+	"https://github.com/ArrowOS-Devices/android_hardware_xiaomi.git -b arrow-12.1 hardware/xiaomi"
 )
 
 # Generate Vendor tree?
-GEN_VT=false
-DUMP_LINK=""
+GEN_VT=true
+DUMP_LINK="https://gitlab.com/Android-Dumps/redmi/evergo.git"
 DUMP_PATH="$HOME/dumps/${DEVICE}"
 
 # Extra Command
 extra_cmd_function(){
+	sed -i 's/rosemary/evergo/g' device/xiaomi/evergo/BoardConfig.mk
 }
 
 EXTRA_CMD=extra_cmd_function;
