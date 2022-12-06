a858af4a9f3bd4e6e73cf1780bf5a0d44193263c
Sushrut1101 <guptasushrut@gmail.com>
2022-12-06 15:41:51 +0530

    Add Initial Config
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

---
+++ config.sh
@@ -0,0 +1,42 @@
+#
+# config.sh
+#
+
+# Manifest
+export MANIFEST=""
+export MANIFEST_BRANCH=""
+
+# Device info
+export DEVICE=""
+export OEM=""
+
+# Lunch combo and Build target
+export LUNCH_COMBO=""
+export TARGET=(
+)
+
+# Device trees and dependencies
+DT_LINK=""
+DT_BRANCH=""
+
+VT_LINK=""
+
+KERNEL_SRC=""
+KERNEL_PATH="kernel/${OEM}/${DEVICE}"
+
+DEPS=(
+)
+
+# Generate Vendor tree?
+GEN_VT=false
+DUMP_LINK=""
+DUMP_PATH="$HOME/dumps/${DEVICE}"
+
+# Extra Command
+extra_cmd_function(){
+}
+
+EXTRA_CMD=extra_cmd_function;
+
+# Common
+export SYNC_PATH="$HOME/work"
