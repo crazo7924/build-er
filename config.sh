9631067a48c13c6ceededf25829f9e4413fd6fa9
Bharat Dev Burman <crazo7924@proton.me>
2023-01-12 23:05:40 +0530

    Update config.sh
    export everything
    gen vendor and set correct dump repository link

--- config.sh
+++ config.sh
@@ -15,24 +15,21 @@ export LUNCH_COMBO="lineage_apollob-userdebug"
 export TARGET="bacon"
 
 # Our output file
-OUTPUT="Lineage-*.zip"
+export OUTPUT="Lineage-*.zip"
 
 # Device trees and dependencies
-DT_LINK="https://github.com/realme-apollob-dev/android_device_realme_apollob.git"
-DT_BRANCH="lineage-20.0"
+export DT_LINK="https://github.com/realme-apollob-dev/android_device_realme_apollob.git"
+export DT_BRANCH="lineage-20.0"
 
-VT_LINK="https://github.com/Mayuri-Chan/proprietary_vendor_xiaomi_garden.git"
-VT_BRANCH= "lineage-20.0"
-
-KERNEL_SRC="https://github.com/realme-apollob-dev/android_kernel_realme_mt6833.git -b android-12.1"
-KERNEL_PATH="kernel/${OEM}/${DEVICE}"
+export KERNEL_SRC="https://github.com/realme-apollob-dev/android_kernel_realme_mt6833.git -b android-12.1"
+export KERNEL_PATH="kernel/${OEM}/${DEVICE}"
 
 # Generate Vendor tree?
-GEN_VT=false
-DUMP_LINK="https://gitlab.com/Android-Dumps/redmi/dandelion.git"
-DUMP_PATH="$HOME/dumps/${DEVICE}"
+export GEN_VT=true
+export DUMP_LINK="https://dumps.tadiphone.dev/dumps/realme/re513cl1.git"
+export DUMP_PATH="$HOME/dumps/${DEVICE}"
 
-EXTRA_CMD=extra_cmd_function;
+export EXTRA_CMD=extra_cmd_function;
 
 # Common
 export SYNC_PATH="$HOME/work"
