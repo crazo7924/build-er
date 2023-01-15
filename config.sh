e069f8550278755e4cb5e39f2dc616c06d93153d
Bharat Dev Burman <crazo7924@proton.me>
2023-01-15 12:21:54 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,33 +3,37 @@
 #
 
 # Manifest
-export MANIFEST="https://github.com/LineageOS/android.git"
-export MANIFEST_BRANCH="lineage-20.0"
+MANIFEST="https://github.com/LineageOS/android.git"
+MANIFEST_BRANCH="lineage-20.0"
 
 # Device info
-export DEVICE="apollob"
-export OEM="realme"
+DEVICE="apollob"
+OEM="realme"
 
 # Lunch combo and Build target
-export LUNCH_COMBO="lineage_apollob-userdebug"
-export TARGET="bacon"
+LUNCH_COMBO="lineage_apollob-userdebug"
+TARGET="bacon"
 
 # Our output file
-export OUTPUT="Lineage-*.zip"
+OUTPUT="lineage-*.zip"
 
 # Device trees and dependencies
-export DT_LINK="https://github.com/realme-apollob-dev/android_device_realme_apollob.git"
-export DT_BRANCH="lineage-20.0"
+DT_LINK="https://github.com/realme-apollob-dev/android_device_realme_apollob.git"
+DT_BRANCH="lineage-20.0"
 
-export KERNEL_SRC="https://github.com/realme-apollob-dev/android_kernel_realme_mt6833.git -b android-12.1"
-export KERNEL_PATH="kernel/${OEM}/${DEVICE}"
+DEPS=(
+	"https://github.com/realme-apollob-dev/android_hardware_oplus.git hardware/oplus",
+        "https://github.com/realme-apollob-dev/android_hardware_mediatek.git hardware/mediatek",
+        "https://github.com/realme-apollob-dev/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr"
+)
+
+KERNEL_SRC="https://github.com/realme-apollob-dev/android_kernel_realme_mt6833.git -b android-12.1"
+KERNEL_PATH="kernel/${OEM}/${DEVICE}"
 
 # Generate Vendor tree?
-export GEN_VT=true
-export DUMP_LINK="https://dumps.tadiphone.dev/dumps/realme/re513cl1.git"
-export DUMP_PATH="$HOME/dumps/${DEVICE}"
-
-export EXTRA_CMD=extra_cmd_function;
+GEN_VT=true
+DUMP_LINK="https://dumps.tadiphone.dev/dumps/realme/re513cl1.git"
+DUMP_PATH="$HOME/dumps/${DEVICE}"
 
 # Common
-export SYNC_PATH="$HOME/work"
+SYNC_PATH="$HOME/work"
