33f6ade43c63441bf043cc4043b67f1d24725b32
Bharat Dev Burman <crazo7924@proton.me>
2023-01-15 13:44:21 +0530

    fixup deps array
    It must be space seperated and not with commas

--- config.sh
+++ config.sh
@@ -22,9 +22,11 @@ DT_LINK="https://github.com/realme-apollob-dev/android_device_realme_apollob.git
 DT_BRANCH="lineage-20.0"
 
 DEPS=(
-	"https://github.com/realme-apollob-dev/android_hardware_oplus.git hardware/oplus",
-        "https://github.com/realme-apollob-dev/android_hardware_mediatek.git hardware/mediatek",
+	"https://github.com/realme-apollob-dev/android_hardware_oplus.git hardware/oplus" 
+        "https://github.com/realme-apollob-dev/android_hardware_mediatek.git hardware/mediatek" 
         "https://github.com/realme-apollob-dev/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr"
+        "https://github.com/realme-apollob-dev/android_packages_apps_RealmeParts.git packages/apps/RealmeParts" 
+        "https://github.com/realme-apollob-dev/android_kernel_realme_mt6833_vendor.git kernel/realme/vendor" 
 )
 
 KERNEL_SRC="https://github.com/realme-apollob-dev/android_kernel_realme_mt6833.git -b android-12.1"
