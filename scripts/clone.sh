de5d5bb315bd74e54b0c7d3d76b85158c816696f
Bharat Dev Burman <crazo7924@proton.me>
2023-01-17 19:13:10 +0530

    clone: switch to home directory

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Source helpers
-for helper in $(ls ${CIRRUS_WORKING_DIR}/helpers/*.sh); do
+for helper in $(ls ${HOME}/helpers/*.sh); do
 	info "Including helpers/${helper}..."
 	source "${helper}"
 done
